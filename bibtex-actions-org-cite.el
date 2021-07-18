ac1bc13485be5657e00dfc51a465538356a617a3
Bruce D'Arcus <bdarcus@gmail.com>
2021-07-18 09:14:53 -0400

    start of style select function

--- bibtex-actions-org-cite.el
+++ bibtex-actions-org-cite.el
@@ -38,10 +38,72 @@
 (require 'org)
 (require 'oc)
 (require 'oc-basic)
+(require 'oc-csl)
+(require 'citeproc)
 (require 'embark)
 
 (declare-function bibtex-actions-at-point "bibtex-actions")
 (declare-function org-open-at-point "org")
+
+;;; Internal variables
+
+(defvar bibtex-actions-org-cite-style-preview-alist
+  '((natbib . bibtex-actions--org-cite-natbib-style-preview)
+    (biblatex . bibtex-actions--org-cite-biblatex-style-preview)))
+
+(defvar bibtex-actions--org-cite-biblatex-style-preview
+  '(;; Default "nil" style.
+    ("/" . "\\autocite")
+    ("/b" . "\\cite")
+    ("/c" . "\\Autocite")
+    ("/bc" . "\\Cite")
+    ;; "text" style.
+    ("t" . "\\textcite")
+    ("t/c" .  "\\Textcite")
+    ;; "nocite" style.
+    ("n" . "\\nocite")
+    ;; "author" style.
+    ("a/c" . "\\Citeauthor*")
+    ("a/f" . "\\citeauthor")
+    ("a/cf" . "\\Citeauthor")
+    ("a" . "\\citeauthor*")
+    ;; "locators" style.
+    ("l/b" . "\\notecite")
+    ("l/c" . "\\Pnotecite")
+    ("l/bc" . "\\Notecite")
+    ("l" . "\\pnotecite")
+    ;; "noauthor" style.
+    ("na" .  "\\autocite*")))
+
+(defvar bibtex-actions--org-cite-natbib-style-preview
+  '(;; Default ("nil") style.
+    ("/" . "\\citep")
+    ("/b" . "\\citealp")
+    ("/c" . "\\Citep")
+    ("/f" . "\\citep*")
+    ("/bc" .  "\\Citealp")
+    ("/bf" . "\\citealp*")
+    ("/cf" . "\\Citep*")
+    ("/bcf" . "\\Citealp*")
+    ;; "text" style.
+    ("t" . "\\citet")
+    ("t/b" . "\\citealt")
+    ("t/c" . "\\Citet")
+    ("t/f" . "\\citet*")
+    ("t/bc" . "\\Citealt")
+    ("t/bf"  .   "\\citealt*")
+    ("t/cf" .    "\\Citet*")
+    ("t/bcf" . "\\Citealt*")
+    ;; "author" style.
+    ("a" . "\\citeauthor")
+    ("a/c" . "\\Citeauthor")
+    ("a/f" . "\\citeauthor*")
+    ;; "noauthor" style.
+    ("na" . "\\citeyearpar")
+    ("na/b" .   "\\citeyear")
+    ;; "nocite" style.
+    ("n" .  "\\nocite")))
+
 
 ;TODO
 ;(defvar bibtex-actions-org-cite-open-default
@@ -64,6 +126,39 @@
            #'bibtex-actions-org-cite-insert
            #'org-cite-basic--complete-style)
   :follow #'bibtex-actions-org-cite-follow)
+
+(defun bibtex-actions-org-cite-select-style (&optional keys)
+"Complete a citation style for org-cite with KEYS preview.
+
+The previews include all configured export processors, with the
+following faces to indicate:
+
+'bibtex-actions-org-cite-styles-default' (default exported processor)
+'bibtex-actions-org-cite-styles-fallback' (a style or variant not directly supported)"
+;; TODO
+)
+
+(defun bibtex-actions--org-cite-styles-completing-read-group (cand transform)
+  "Return group title of style CAND or TRANSFORM the candidate."
+  (if transform
+      ;; If there's a variant, remove it from the group title.
+      (car (split-string cand "/")))
+    cand)
+
+(defun bibtex-actions-csl-render-citation (citation)
+  "Render CITATION."
+  (let ((proc (org-cite-csl--processor)))
+    (citeproc-clear proc)
+    (let* ((info (list :cite-citeproc-processor proc))
+	   (cit-struct (org-cite-csl--create-structure citation info)))
+      (citeproc-append-citations (list cit-struct) proc)
+      (car (citeproc-render-citations proc 'plain t)))))
+
+(defun bibtex-actions-org-cite-style-to-command (style output)
+  "Return comand from STYLE for OUTPUT."
+  ;; This should be added to oc.el.
+  (let styles (assoc output bibtex-actions-org-cite-style-preview-alist)
+       (cdr (assoc style styles))))
 
 ;;; Embark target finder
 
